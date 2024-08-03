62db8227ec1bbda22865e0b168ae4bfc63259308
errbchile <errbchile@gmail.com>
2024-08-03 14:02:17 +0200

    Character Card

---
+++ src/components/HeaderSection/HeaderSection.tsx
@@ -0,0 +1,8 @@
+import logo from "../../assets/logo.svg";
+import heart from "../../assets/heart.svg";
+
+export default function Header() {
+  return (
+   <></>
+  );
+}
---
+++ src/components/SearchSection/SearchSection.tsx
@@ -0,0 +1,7 @@
+import loupe from "../../assets/loupe.svg";
+
+export default function SearchSection() {
+  return (
+  <></>
+  );
+}
